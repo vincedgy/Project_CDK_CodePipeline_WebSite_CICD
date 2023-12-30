ff85fd47ec2fe66f277932daedac02cca62df5d0
Vincent <vincent.dagoury@gmail.com>
2023-12-30 09:31:13 +0100

    Adding .dev.example.yaml

--- utils/index.ts
+++ utils/index.ts
@@ -3,16 +3,24 @@ import * as dotenv from "dotenv";
 import * as path from "path";
 import * as yaml from "yaml";
 
+/**
+ * Returns an Object containing all the setting depending on mode
+ * @param mode, either 'dev' or 'prod'
+ * @param envDirPath, default is "env" dir
+ * @returns Object
+ */
 export const loadEnvironmentVariablesFile = (
   mode: "dev" | "prod",
   envDirPath = path.join(process.cwd(), "env")
 ) => {
+  // Base is the common part
   const baseYaml = fs.readFileSync(path.join(envDirPath, ".base.yaml"), "utf-8");
   const modeYaml = fs.readFileSync(
     path.join(envDirPath, mode === "prod" ? ".prod.yaml" : ".dev.yaml"),
     "utf-8"
   );
   
+  // Build a global object containing all env variable in on single Object
   const baseObj = yaml.parse(baseYaml);
   const modeObj = yaml.parse(modeYaml);
   return Object.assign({}, baseObj, modeObj);
